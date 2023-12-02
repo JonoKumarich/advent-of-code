33310570a7df59962e5c0f7ab8565c31cd81afc3
JonoKumarich <jonathankumarich@gmail.com>
2023-12-02 18:20:22 +0100

    Day 02 part 02

--- answers/day_02/answer.py
+++ answers/day_02/answer.py
@@ -5,14 +5,18 @@ MAX_COLORS = {
 }
 
 
-def part_01(input: str) -> str:
+def get_games(input: str) -> dict[str, str]:
     lines = [line.split(': ') for line in input.splitlines()]
     games = {game[0].split(' ')[1]: game[1].split('; ') for game in lines}
     
-    total_ids = 0
+    return games
+
+
+def part_01(input: str) -> str:
+    games = get_games(input)
     
+    total_ids = 0
     for game in games:
-        
         game_is_valid = True
         
         for round in games[game]:            
@@ -25,9 +29,29 @@ def part_01(input: str) -> str:
         if game_is_valid:
             total_ids += int(game)
             
-            
     return total_ids
 
 
 def part_02(input: str) -> str:
-    return "Part two answer"
+    games = get_games(input)
+    total = 0
+    
+    for game in games:
+        color_counts = {
+            'red': 0,
+            'blue': 0,
+            'green': 0        
+        }
+        
+        for round in games[game]:
+            for color in round.split(', '):
+                number, color_value = color.split(' ')
+                
+                color_counts[color_value] = max(color_counts[color_value], int(number))
+        
+        total += color_counts['blue'] * color_counts['red'] * color_counts['green']
+    
+    return total
+    
+    
+        
