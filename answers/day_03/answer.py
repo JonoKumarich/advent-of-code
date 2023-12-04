8c24d1d3deaa1bfb8fb18b5fb7d09f992b7073db
JonoKumarich <jonathankumarich@gmail.com>
2023-12-04 14:52:54 +0100

    Day 03 part 02

--- answers/day_03/answer.py
+++ answers/day_03/answer.py
@@ -45,17 +45,173 @@ def part_01(input: str) -> str:
                 checked_js.extend(list(range(j,j+number_length)))
                 
     return total_part_numbers
-                
-                
-                
-                    
 
-                
-            
-            # Need to somehow skip the whole number when first digit processed
-            
-    
-    return schematic
 
 def part_02(input: str) -> str:
-    return "Part two answer"
+    '''
+    Assumptions: Only ever 2 numbers surrounding a gear
+    
+    Locate all potential gear indices
+    For each gear index, locate surrounding numbers
+    Count:
+        Num vals above
+        Num vals below
+        Num vals left
+        Num vals right
+    If 2 categories have vals, then collect numbers based on location
+    If just top or bottom, then check if numbers are connected or separate
+    '''
+    
+    # Answer has too much duplication in walk left / right logic. Let's clean that up in the future.
+    
+    schematic = [list(line) for line in input.splitlines()]
+    total = 0
+    
+    # Get gear locations
+    star_locations = [[(i, j) for j, val in enumerate(row) if val == "*"] for i, row in enumerate(schematic)]
+    potential_gears = []
+    for row in star_locations:
+        potential_gears.extend(row)
+    
+    for gear in potential_gears:
+        
+        neighbours = []
+        neighbours.extend(get_row_numbers(schematic, gear, -1)) # Top row
+        neighbours.extend(get_row_numbers(schematic, gear, 1)) # Bottom row
+        
+        # Walk right
+        if schematic[gear[0]][gear[1]-1].isdigit():
+            number = ''
+            
+            left_index = gear[1] - 1
+            while True:
+                if left_index < 0:
+                    break
+                
+                val = schematic[gear[0]][left_index]
+                
+                if not val.isdigit():
+                    break 
+                
+                number = val + number
+                left_index -= 1
+                
+            neighbours.append(number)
+        
+        # Walk right
+        if schematic[gear[0]][gear[1]+1].isdigit():
+            number = ''
+            
+            right_index = gear[1] + 1
+            while True:
+                if right_index > len(schematic[0]) - 1:
+                    break
+                
+                val = schematic[gear[0]][right_index]
+                
+                if not val.isdigit():
+                    break 
+                
+                number = number + val
+                right_index += 1
+                
+            neighbours.append(number)
+        
+        print(gear, neighbours)
+        if len(neighbours) == 2:
+            total += int(neighbours[0]) * int(neighbours[1])
+        
+    return total
+
+
+def get_row_numbers(schematic: list[list[str]], gear: tuple[int, int], offset: int) -> list[int]:
+    
+    if offset not in [-1, 1]:
+        raise ValueError(f"Invalid offset {offset}")
+    
+    is_top = gear[0] == 0
+    is_bottom = gear[0] == len(schematic) - 1
+    
+    if (offset == -1 and is_top) or (offset == 1 and is_bottom):
+        return []
+    
+    left, middle, right = schematic[gear[0]+offset][gear[1]-1:gear[1]+2]
+    
+    if not any([left.isdigit(), middle.isdigit(), right.isdigit()]):
+        return []
+    
+    # If middle then walk both ways - can return
+    if middle.isdigit():
+        number = middle
+        
+        
+        # Walk right
+        right_index = gear[1] + 1
+        while True:
+            if right_index > len(schematic[0]) - 1:
+                break
+            
+            val = schematic[gear[0]+offset][right_index]
+            
+            if not val.isdigit():
+                break 
+            
+            number = number + val
+            right_index += 1
+        
+        # Walk left
+        left_index = gear[1] - 1
+        while True:
+            if left_index < 0:
+                break
+            
+            val = schematic[gear[0]+offset][left_index]
+            
+            if not val.isdigit():
+                break 
+            
+            number = val + number
+            left_index -= 1
+            
+        return [number]
+    
+    numbers = []
+    
+    # If left, then walk left
+    if left.isdigit():
+        number = ''
+        left_index = gear[1] - 1
+
+        while True:
+            if left_index < 0:
+                break
+            
+            val = schematic[gear[0]+offset][left_index]
+
+            if not val.isdigit():
+                break 
+            
+            number = val + number
+            left_index -= 1
+        
+        numbers.append(number)
+        
+    # If right, then walk right
+    if right.isdigit():
+        number = ''
+        right_index = gear[1] + 1
+        while True:
+            if right_index > len(schematic[0]) - 1:
+                break
+            
+            val = schematic[gear[0]+offset][right_index]
+            
+            if not val.isdigit():
+                break 
+            
+            number = number + val
+            right_index += 1
+        
+        numbers.append(number)
+    
+    return numbers
