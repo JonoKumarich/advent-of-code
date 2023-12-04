be5f72930226adcfbd66ecc8f0e27d9aaff1573e
JonoKumarich <jonathankumarich@gmail.com>
2023-12-04 16:50:01 +0100

    Day 01 refactor

--- answers/day_01/answer.py
+++ answers/day_01/answer.py
@@ -1,51 +1,42 @@
 import re
 
+NUM_REGEX = r"[^0-9]"
+TEXT_NUM_REGEX = r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"
+
 def part_01(input: str) -> str:
-    numbers = [re.sub("[^0-9]", "", line) for line in input.splitlines()]
-    total = 0
-    
-    for line in numbers:
-        total += int(line[0] + line[-1])
-        
-    return str(total)
+    numbers = [re.sub(NUM_REGEX, "", line) for line in input.splitlines()]
+    return sum(join_first_and_last_digits(digits) for digits in numbers)
 
 def part_02(input: str) -> str:
-    # Assumption that no duplicates. If doesn't work, then lets fix. --> NEED TO FIX THIS
     
-    numbers = {
-        'one': 1, 
-        'two': 2, 
-        'three': 3, 
-        'four': 4, 
-        'five': 5, 
-        'six': 6, 
-        'seven': 7, 
-        'eight': 8, 
-        'nine': 9,
-        '1': 1, 
-        '2': 2, 
-        '3': 3, 
-        '4': 4, 
-        '5': 5, 
-        '6': 6, 
-        '7': 7, 
-        '8': 8, 
-        '9': 9
-    }
+    digit_lines = [
+        [get_digit(digit) for digit in re.findall(TEXT_NUM_REGEX, line)] 
+        for line in input.splitlines()
+    ]
     
-    total = 0
+    return sum(join_first_and_last_digits(digits) for digits in digit_lines)
     
-    for line in input.splitlines():
-        first_occurance = {number: line.find(number) for number in numbers if line.find(number) != -1}
-        last_occurance = {number: line.rfind(number) for number in numbers if line.find(number) != -1}
-        
-        first_number = numbers[sorted(first_occurance.items(), key=lambda x:x[1])[0][0]]
-        last_number = numbers[list(reversed(sorted(last_occurance.items(), key=lambda x:x[1])))[0][0]] 
-        
-        
-        final_number = str(first_number) + str(last_number)
-        print(final_number)
-        total += int(final_number)
-        
-        
-    return str(total)
+
+def get_digit(number_text: str) -> int:
+    '''Converts numbers to integers from both str and English text form.'''
+    try:
+        return {
+            'one': 1, 
+            'two': 2, 
+            'three': 3, 
+            'four': 4, 
+            'five': 5, 
+            'six': 6, 
+            'seven': 7, 
+            'eight': 8, 
+            'nine': 9,
+        }[number_text]
+    except KeyError:
+        return int(number_text)
+
+
+def join_first_and_last_digits(digits: list[int]) -> int:
+    '''Joins first and last digits together into an integer'''
+    joined_string = str(digits[0]) + str(digits[-1])
+    return int(joined_string)
+    
