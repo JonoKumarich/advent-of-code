b6a9212eed4f2c0ea15b5c5d148b4c9aa4e61928
JonoKumarich <jonathankumarich@gmail.com>
2023-12-05 22:47:47 +0100

    Outputting somewhat correct values ready to format

--- answers/day_05/answer.py
+++ answers/day_05/answer.py
@@ -1,5 +1,3 @@
-from tqdm import tqdm
-
 ValueMap = list[tuple[int, int, int]]
 
 
@@ -20,18 +18,22 @@ def part_02(input: str) -> str:
     seed_ranges = [(seed_start, seed_info[i+1]) for i, seed_start in enumerate(seed_info) if i % 2 == 0]
     maps = build_maps(groups)
     
-    # We need to try and process in bulk here
-    # Let's try and think in terms of seed ranges
-    # If a portion of a seed range fits into a single mapping range, 
-    # then we can simply shift the whole range by the difference from source -> destination
-    bulk_seed_mappings = []
-    for seed_start, range_size in seed_ranges:
-        # Get bulk ranges for seeds
-        bulk_seed_mappings.extend(bulk_seed_map(seed_start, range_size, maps))
+    for value_map in maps.values():
+        bulk_seed_mappings = []
+        for seed_start, range_size in seed_ranges:
+            # Get bulk ranges for seeds
+            bulk_seed_mappings.extend(bulk_seed_map(seed_start, range_size, value_map))
         
-    return bulk_seed_mappings
+        seed_ranges = []
+        for mapping in bulk_seed_mappings:
+            bin = mapping[2]
+            offset = 0 if bin is None else value_map[bin][0] - value_map[bin][1]
+            
+            # Updated seed_ranges with new bulk mapping translations
+            seed_ranges.append((mapping[0] + offset, mapping[1] + offset))
         
-        
+    return seed_ranges
+
 
 def get_map(groups: list[str], index: int) -> ValueMap:
     return [
@@ -59,7 +61,7 @@ def get_mapped_value(input_value: int, map: ValueMap) -> int:
 
 def map_seeds(seeds: list[int], maps: list[ValueMap]) -> list[int]:
     location_values = []
-    for seed in tqdm(seeds):
+    for seed in seeds:
         value = seed
         for map in maps:
             value = get_mapped_value(value, map)
@@ -74,13 +76,15 @@ def build_maps(groups: list[str]):
     return {get_map_name(groups, index): get_map(groups, index) for index in map_ranges}
 
 
-def bulk_seed_map(seed_start: int, seed_range: int, maps: list[ValueMap]) -> list[list[int]]:
+def bulk_seed_map(seed_start: int, seed_range: int, value_map: ValueMap) -> list[list[int]]:
+
+    
     current_seed = seed_start
     bulk_seed_maps = []
     
     while current_seed < seed_start + seed_range:
         
-        for bin, (_, source_start, length) in enumerate(maps['seed-to-soil']):
+        for bin, (_, source_start, length) in enumerate(value_map):
             
             if current_seed in range(source_start, source_start + length):
                 bulk_seed_maps.append([current_seed, min(source_start + length, seed_start + seed_range) - 1, bin])
@@ -88,9 +92,13 @@ def bulk_seed_map(seed_start: int, seed_range: int, maps: list[ValueMap]) -> lis
                 
                 break
             
-            if bin == len(maps['seed-to-soil']) - 1:
-                # only if in last element of loop
-                next_bin = min(bin[1] for bin in maps['seed-to-soil'] if bin[1] > current_seed)
+            if bin == len(value_map) - 1:
+                # FIXME: It fucks out on the first pass, second map - here
+                # Because when last value have no bin, it can't find the min of the next bin, should return end of 
+                try:
+                    next_bin = min(bin[1] for bin in value_map if bin[1] > current_seed)
+                except ValueError:
+                    next_bin = seed_start + seed_range
                 bulk_seed_maps.append([current_seed, next_bin, None])
                 current_seed = next_bin
 
