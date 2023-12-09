6da11f3c853ece036a71b7e9948346f9d4740616
JonoKumarich <jonathankumarich@gmail.com>
2023-12-09 09:37:49 +0100

    Day 09 part 02

--- 2023/python/answers/day_09/answer.py
+++ 2023/python/answers/day_09/answer.py
@@ -2,7 +2,7 @@ from itertools import pairwise
 
 
 def part_01(input: str) -> int:
-    readings = [list(map(int, line.split())) for line in input.splitlines()]
+    readings = parse_input(input)
     last_readings = [[line[-1]] for line in readings]
     
     for i, line in enumerate(readings):
@@ -15,5 +15,29 @@ def part_01(input: str) -> int:
     return sum(sum(history) for history in last_readings)
 
 
-def part_02(input: str) -> str:
-    return "Part two answer"
+def part_02(input: str) -> int:
+    readings = parse_input(input)
+    first_readings = [[line[0]] for line in readings]
+    for i, line in enumerate(readings):
+        values: list[int] = [1]
+        while not all(val == 0 for val in values):
+            values = [b - a for (a, b) in pairwise(line)]
+            first_readings[i].insert(0, values[0])
+            line = values
+    
+    values = []
+    for reading in first_readings:
+        total = 0
+        for value in reading:
+            total = value - total
+            
+        values.append(total)
+    
+    return sum(values)
+
+
+def parse_input(input: str) -> list[list[int]]:
+    return [
+        [int(value) for value in line.split()]
+        for line in input.splitlines()
+    ]
