ad6e5823f405436e2e40f9fd060945ed4371fe70
JonoKumarich <jonathankumarich@gmail.com>
2023-12-11 23:39:03 +0100

    Day 11 part 02

--- 2023/python/answers/day_11/answer.py
+++ 2023/python/answers/day_11/answer.py
@@ -22,15 +22,48 @@ def part_01(input: str) -> int:
     return total
         
     
-                
-
-
 def part_02(input: str) -> str:
-    return "Part two answer"
+    """
+    Steps:
+        - Do not expant universe
+        - Get indexes in which contain no galaxies
+        - For each point:
+            - calculate manhatten distance
+            - calculate number of times it crosses these indexes
+                - add # indexes x expansion multiplier and add to manhatten distance
+    """
+    
+    grid = [list(line) for line in input.splitlines()]
+    
+    clear_horizontal: list[int] = []
+    for i in range(len(grid)):
+        if all([val == '.' for val in grid[i]]):
+            clear_horizontal.append(i)
+    
+    clear_vertical: list[int] = []
+    for i in range(len(grid[0])):
+        if all ([row[i] == '.' for row in grid]):
+            clear_vertical.append(i)
+    
+    galaxies: list[tuple[int, int]] = []
+    for r, row in enumerate(grid):
+        for c, val in enumerate(row):
+            if val == '#':
+                galaxies.append((r, c))
+    
+    total = 0
+    for a, b in combinations(galaxies, 2):
+        MULTIPLIER = 1000000 - 1
+        total += len([val for val in clear_horizontal if val > min(a[0], b[0]) and val < max(a[0], b[0])]) * MULTIPLIER
+        total += len([val for val in clear_vertical if val > min(a[1], b[1]) and val < max(a[1], b[1])]) * MULTIPLIER
+        
+        total += manhattan(a, b)
+    
+    
+    return total
 
 
 def expand_universe_vertically(universe: list[list[str]]) -> list[list[str]]:
-    
     clear_horizontal: list[int] = []
     for i in range(len(universe)):
         if all([val == '.' for val in universe[i]]):
