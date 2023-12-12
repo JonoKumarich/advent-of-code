1dba9358295c2dfaab7d9f297a84611a0f49db74
JonoKumarich <jonathankumarich@gmail.com>
2023-12-12 17:49:45 +0100

    Day 12 part 02

--- 2023/python/answers/day_12/answer.py
+++ 2023/python/answers/day_12/answer.py
@@ -1,40 +1,88 @@
 from itertools import combinations
+from functools import lru_cache
 
 
-def part_01(input: str) -> int:
-    '''
-    Let's try brute force:
-    
-    For each row:
-        - Calculate number of #'s
-        - Calculate number of remaining #'s to allocate
-        - For each combination of #'s:
-            - Check if that combination then matches the arrangement key
-    '''
-    
+NUM_REPEATS = 5
+
+
+def part_01(input: str) -> int:    
     lines = [line.split() for line in input.splitlines()]
     arrangements = [list(map(int, arrangement.split(','))) for _, arrangement in lines]
     records = [record for record, _ in lines]
     
     total = 0
     for record, arrangement in zip(records, arrangements):
-        existing_hashes = record.count('#')
-        allocatable_hashes = sum(arrangement) - existing_hashes
-        variable_indexes = [i for i, ltr in enumerate(record) if ltr == '?']
         
-        for combos in combinations(variable_indexes, allocatable_hashes):
-            potential_record = ['#' if n in combos else c for n, c in enumerate(record)]
-            potential_record = ''.join(potential_record).replace('?', '.')
-            
-            if get_hash_groups(potential_record) == arrangement:
-                total += 1
+        total += calculate_record(tuple(record), tuple(arrangement))
                 
     return total
 
 
-def part_02(input: str) -> str:
-    return "Part two answer"
+def part_02(input: str) -> int:
+    lines = [line.split() for line in input.splitlines()]
+    arrangements = [list(map(int, arrangement.split(','))) for _, arrangement in lines]
+    records = [record for record, _ in lines]
+    
+    total = 0
+    for record, arrangement in zip(records, arrangements):
+        repeated_record: list[str] = []
+        repeated_arrangement: list[int] = []
+        for i in range(NUM_REPEATS):
+            if i != 0:
+                repeated_record.append('?')
+            repeated_record.extend(record)
+            repeated_arrangement.extend(arrangement)    
+        
+        add = calculate_record(tuple(repeated_record), tuple(repeated_arrangement))
+        total += add
+        
+    return total
+    
+# Optimisations:
 
+@lru_cache
+def calculate_record(remaining_record: tuple[str], remaining_arrangement: tuple[int]) -> int:
+    remaining_record = list(remaining_record)
+    remaining_arrangement = list(remaining_arrangement)
+    
+    if len(remaining_arrangement) == 0:
+        if any(record == '#' for record in remaining_record):
+            return 0
+        return 1
+    
+    if len(remaining_record) == 0:
+        return 0
+    
+    match remaining_record[0]:
+        case '.':
+            return calculate_record(tuple(remaining_record[1:]), tuple(remaining_arrangement))
+        case '?':
+            dot_result = calculate_record(tuple(['.'] + remaining_record[1:]), tuple(remaining_arrangement))
+            hash_result = calculate_record(tuple(['#'] + remaining_record[1:]), tuple(remaining_arrangement))
+            return dot_result + hash_result
+            
+        case '#':
+            # Check for continuous string possible long enough
+            if any([val == '.' for val in remaining_record[:remaining_arrangement[0]]]):
+                return 0
+            
+            if remaining_arrangement[0] > len(remaining_record):
+                return 0
+            
+            if remaining_arrangement[0] == len(remaining_record):
+                remaining_arrangement.pop(0)
+                return calculate_record((), tuple(remaining_arrangement))
+            
+            if remaining_record[remaining_arrangement[0]] == '#':
+                return 0
+            
+            return calculate_record(tuple(remaining_record[remaining_arrangement[0]+1:]), tuple(remaining_arrangement)[1:])
+        case _:
+            raise ValueError(f"Invalid value found: '{remaining_record[0]}'")
+        
+    
+    
+    
 
 def get_hash_groups(record: str) -> list[int]:
     char_groups: list[int] = []
