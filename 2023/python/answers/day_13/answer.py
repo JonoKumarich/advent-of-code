7c923038b7d51c8a51f47a6faafcedc3e129ea54
JonoKumarich <jonathankumarich@gmail.com>
2023-12-14 09:23:30 +0100

    Day 13 part 02

--- 2023/python/answers/day_13/answer.py
+++ 2023/python/answers/day_13/answer.py
@@ -5,8 +5,14 @@ def part_01(input: str) -> int:
     for block in blocks:
         horizontal_reflection = find_mirror_index(block, 0) 
         vertical_reflection = find_mirror_index(reverse_block(block), 0)
-        total += 100 * horizontal_reflection
-        total += vertical_reflection
+        print(horizontal_reflection, vertical_reflection)
+
+        if horizontal_reflection is not None:
+            total += 100 * horizontal_reflection
+
+        if vertical_reflection is not None:
+            total += vertical_reflection
+
     return total
 
 def part_02(input: str) -> int:
@@ -14,15 +20,42 @@ def part_02(input: str) -> int:
     total = 0
 
     for block in blocks:
-        horizontal_reflection = find_mirror_index(block, 1) 
-        vertical_reflection = find_mirror_index(reverse_block(block), 1)
-        # TODO: Return correct value of the two as both can be non-zero
-        # TODO: Handle case when both values equal. Let's assume that the smudge case would be better
-        # We need to cause a different reflection line for each one
-        print(blocks.index(block), horizontal_reflection, vertical_reflection, find_mirror_index(block), find_mirror_index(reverse_block(block)))
+        old_reflections = (find_mirror_index(block, 0), find_mirror_index(reverse_block(block), 0))
+
+        original_block = block[:]
+        unique_reflections = {old_reflections}
+        for i in range(len(block[0])):
+            for j in range(len(block)):
+                block = [list(row) for row in block]
+                block[j][i] = '#' if block[j][i] == '.' else '#'
+                block = [''.join(row) for row in block]
+                new_reflections = (
+                    find_mirror_index(block, 0, old_reflections[0]), 
+                    find_mirror_index(reverse_block(block), 0, old_reflections[1])
+                )
+                block = original_block[:]
+                unique_reflections.add(new_reflections)
+                
+        new_reflections =([
+            reflection for reflection in unique_reflections 
+            if reflection != old_reflections and reflection != (None, None)
+        ])[0]
+
+        if new_reflections[0] is not None:
+            total += 100 * new_reflections[0]
+
+        if new_reflections[1] is not None:
+            total += new_reflections[1]
+       
     return total
 
-def find_mirror_index(block: list[str], num_smudges: int = 0) -> int:
+
+def find_mirror_index(
+    block: list[str], 
+    num_smudges: int = 0, 
+    skip_row: int | None = None
+) -> int | None:
+    # FIXME: Revert back to using num_smudges instead of brute force
 
     prev_row = ''
 
@@ -31,7 +64,7 @@ def find_mirror_index(block: list[str], num_smudges: int = 0) -> int:
         if num_differences <= num_smudges and i > 0:
             num_smudges -= num_differences
 
-            if i == 1:
+            if i == 1 and i != skip_row:
                 return 1
 
             max_range = min(i - 1, len(block) - (i + 1))
@@ -43,12 +76,12 @@ def find_mirror_index(block: list[str], num_smudges: int = 0) -> int:
                 else:
                     num_smudges -= num_differences
 
-            if not break_loop:
+            if not break_loop and i != skip_row:
                 return i
 
         prev_row = row
 
-    return 0
+    return None
 
 
 def reverse_block(block: list[str]) -> list[str]:
