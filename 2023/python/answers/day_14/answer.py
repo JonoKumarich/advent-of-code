bbc9a19ca02e41d4693c42c5f7a5bae01bb9986d
JonoKumarich <jonathankumarich@gmail.com>
2023-12-16 16:19:50 +0100

    Day 14 part 02

--- 2023/python/answers/day_14/answer.py
+++ 2023/python/answers/day_14/answer.py
@@ -1,45 +1,95 @@
 def part_01(input: str) -> int:
+    grid = [list(line) for line in input.splitlines()]
     total_weight = 0
-
-    for column in parse_input(input):
-        for i, val in enumerate(column):
-            if i == 0:
-                continue
-            
-            is_unmovable = val in ['.', '#']
-            if is_unmovable:
-                continue
-            
-            is_blocked = column[i - 1] in ['O', '#']
-            if is_blocked:
-                continue
-            
-            for j in reversed(range(i)):
-                # J = next spot, J - 1 = current spot
-                next_is_free = column[j] == '.'
-                
-                if j == 0 and column[0] == '.':
-                    column[j] = 'O'
-                    column[i] = '.'    
-                    break
-                
-                if next_is_free:
-                    continue
-                
-                column[j + 1] = 'O'
-                column[i] = '.'
-                break
-        
+    
+    
+    for column in [list(x) for x in zip(*grid)]:
+        column = roll_rocks(column)
         max_distance = len(column)
         rock_distances = [max_distance - i for i, rock in enumerate(column) if rock == 'O']
         total_weight += sum(rock_distances)
         
     return total_weight
 
+
 def part_02(input: str) -> int:
-    return 0
+    grid = [list(line) for line in input.splitlines()]
+    
+    for n in range(1, 100000):
+        # Roll north
+        new_grid: list[list[str]] = []
+        for column in [list(x) for x in zip(*grid)]:
+            new_grid.append(roll_rocks(column))
+        # Untranspose
+        grid = [list(x) for x in zip(*new_grid)]
+        
+        # print_grid(grid, rotations=(n % 4))
+        
+        if n % 4 == 0:
+            rotated_grid = grid[:]
+            for _ in range(3):
+                rotated_grid = [list(x) for x in zip(*grid[::-1])]
+            
+            total_weight = 0
+            for column in [list(x) for x in zip(*rotated_grid)]:
+                max_distance = len(column)
+                rock_distances = [max_distance - i for i, rock in enumerate(column) if rock == 'O']
+                total_weight += sum(rock_distances)
+                
+            print(f'Cycle: {int(n / 4)} = {total_weight}')
+        
+        # Rotate next
+        grid = [list(x) for x in zip(*grid[::-1])]
+        
+    '''
+    Cycle length == 9
+    round(1000000000 / 9) = 111111111
+    1000000000 - 111111111 * 9 = 1
+    Answer will be at cycle # % 9 = 1
+    Manually found result = 96317
+    TODO: Actually automate this part
+    '''
+    
+    raise NotImplementedError
+    
+
+def roll_rocks(column: list[str]) -> list[str]:
+    '''This rolls rocks fron right to left in a column'''
+    for i, val in enumerate(column):
+        if i == 0:
+            continue
+        
+        is_unmovable = val in ['.', '#']
+        if is_unmovable:
+            continue
+        
+        is_blocked = column[i - 1] in ['O', '#']
+        if is_blocked:
+            continue
+        
+        for j in reversed(range(i)):
+            next_is_free = column[j] == '.'
+            
+            if j == 0 and column[0] == '.':
+                column[j] = 'O'
+                column[i] = '.'    
+                break
+            
+            if next_is_free:
+                continue
+            
+            column[j + 1] = 'O'
+            column[i] = '.'
+            break
+    
+    return column
 
 
-def parse_input(input: str) -> list[list[str]]:
-    vertical = [list(line) for line in input.splitlines()]
-    return [list(x) for x in zip(*vertical)] 
+def print_grid(grid: list[list[str]], rotations: int = 0) -> None:
+    for _ in range(3 + rotations):
+        grid = [list(x) for x in zip(*grid)][::-1]
+    
+    for row in grid:
+        print(''.join(row))
+        
+    print()
