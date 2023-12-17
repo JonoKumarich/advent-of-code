baf5c518a679d91a12068a50a4ad1bd6b711cc9f
JonoKumarich <jonathankumarich@gmail.com>
2023-12-17 21:33:43 +0100

    Dijkstra's with 3 limit but can't handle splits

--- 2023/python/answers/day_17/answer.py
+++ 2023/python/answers/day_17/answer.py
@@ -13,6 +13,11 @@ def part_01(input: str) -> int:
         for (i, j) in product(range(height), range(width))
     }
     
+    roll_history: dict[Node, list[Node]] = {
+        (i, j): []
+        for (i, j) in product(range(height), range(width))
+    }
+    
     node = (0, 0)
     final_node = (height - 1, width - 1)
     distances[node] = 0
@@ -28,11 +33,27 @@ def part_01(input: str) -> int:
         unvisited.remove(node)
         
         for next_node in find_neighbouring_nodes(node, (height, width)):
+            # # Can't go back on itself
+            # if next_node == roll_history[node][-1]:
+            #     continue
+            
+            if is_more_than_three_rolls(roll_history[node][-3:], next_node):
+                continue
+            
             potential_distance = distances[node] + grid[next_node[0]][next_node[1]]
             
             if potential_distance < distances[next_node]:
+                roll_history[next_node] = roll_history[node] + [node]
                 distances[next_node] = potential_distance
                 
+    print_grid = [[str(val) for val in row] for row in grid]
+    
+    for roll in roll_history[(12, 12)]:
+        print_grid[roll[0]][roll[1]] = ' '
+        
+    for row in print_grid:
+        print(''.join(row))
+        
     return int(distances[(height - 1, width - 1)])
 
             
@@ -40,7 +61,6 @@ def get_closest_node(unvisited: set[Node], distances: dict[Node, float]) -> Node
     unvisited_distances = {k: v for (k, v) in distances.items() if k in unvisited}
     return sorted(unvisited_distances.items(), key=lambda item: item[1])[0][0]
             
-    
     
 def find_neighbouring_nodes(
     node: Node, 
@@ -60,4 +80,21 @@ def find_neighbouring_nodes(
             filtered_nodes.add(node)
     
     return filtered_nodes
+    
+    
+def is_more_than_three_rolls(roll_history: list[Node], next_roll: Node) -> bool:
+    all_rolls = roll_history + [next_roll]
+    heights = [roll[0] for roll in all_rolls]
+    widths = [roll[1] for roll in all_rolls]
+    
+    if max(heights) - min(heights) >= 4:
+        return True
+    
+    if max(widths) - min(widths) >= 4:
+        return True
+    
+    return False
+
+
+# def dijkstra(grid: list[list[int]], starting_position: Node, roll_history: dict[Node, list[Node]]) -> int:
     
